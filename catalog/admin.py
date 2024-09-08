572ba6e95148eb765ef7af050769321d53b00ab3
reichikku <bladeryz@gmail.com>
2024-09-08 14:44:48 +0300

    Registered models in admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Tag, Task
 
-# Register your models here.
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("content", "datetime", "deadline", "is_done")
+    list_filter = ("is_done", "tags")
+    search_fields = ("content",)
+    date_hierarchy = "datetime"
