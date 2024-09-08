de602bcba39d4cda24a7d263c43c258c8398be0a
reichikku <bladeryz@gmail.com>
2024-09-08 17:13:42 +0300

    Improved the CSS and changed the sorting once again

--- catalog/models.py
+++ catalog/models.py
@@ -16,7 +16,7 @@ class Task(models.Model):
     tags = models.ManyToManyField(Tag, related_name="tasks", blank=True)
 
     class Meta:
-        ordering = ["-is_done", "datetime"]
+        ordering = ["-datetime", "-is_done"]
 
     def __str__(self):
         return self.content
