9fc7fa1788f4cb2a5404c45a65fc930eda634f01
reichikku <bladeryz@gmail.com>
2024-09-08 14:16:19 +0300

    Started implementing templates: added base.html, a sidebar, index.html and tag_list.html, and also added TaskListView and TagListView

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from catalog.views import TaskListView
+from catalog.views import TaskListView, TagListView
 
 urlpatterns = [
-    path("", TaskListView.as_view(), name="index")
+    path("", TaskListView.as_view(), name="index"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
 ]
 
 app_name="catalog"
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,16 @@
 from django.urls import reverse_lazy
 from django.views import generic
 
+from catalog.models import Task, Tag
+
 
 class TaskListView(generic.ListView):
     model = Task
-    template_name = "tasks/index.html"  # Your template
+    template_name = "catalog/index.html"  # Your template
     context_object_name = "tasks"
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    template_name = "catalog/tag_list.html"
+    context_object_name = "tags"
